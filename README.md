39697e096a42266249b6ec70703400fad8bb0930
sabrinaMac <sabrina.scherma@gmail.com>
2022-09-30 10:44:16 -0600

    worked on Player factory function and game initialization

--- README.md
+++ README.md
@@ -10,25 +10,21 @@ DONE - Create a Gameboard module that holds a gameBoard array.
     DONE - Create a reset game function that resets all DOM elements. 
 
 Create a Players factory function to generate the two player objects.
-    X and O are players. Let user select which they want, two players, or computer takes the other. 
+    DONE - X and O are players. Let user select which they want, two players, or computer takes the other. 
     Let players enter their name(s).
 
 Create a Game module to control the gameplay.
     Game starts, and alternates between players. X starts. 
-        As moves are made, you cannot play an occupied square. 
+       DONE - As moves are made, you cannot play an occupied square. 
         Computer makes random moves to available squares. 
     DONE - Create a function inside this module that injects the gameBoard array into the HTML. Do this by making the array elements spaces that take up 1 sqaure on a 3x3 grid. 
 
 
 PROGRESS THOUGHTS - NEXT STEPS 
-1. I want to create a function that alternates which player is making moves. I think this will be amending the Playgame object, firing a check for what value X is at (0 or 1 for playerone and playertwo). 
+DONE 1. I want to create a function that alternates which player is making moves. I think this will be amending the Playgame object, firing a check for what value X is at (0 or 1 for playerone and playertwo). 
 
-2. Create a CreatePlayer factory function that sets the name fields in the HTML (create a field that takes a name for player two as well). When the people click a button next to the name form it should remove the form and add the name in a fancy font. 
+2. Create a CreatePlayer factory function that sets the name fields in the HTML (create a field that takes a name for player two as well). When the people click a button next to the name form it should remove the form and add the name in a fancy font. I'm going to add a function to Gameboard that replaces the form fields with the names, but the form fields need to be hidden not removed so when the game is reset it still keeps the old names. 
     2a. Playertwo should have a radio button that lets you switch between computer and human. 
     2b. These player objects should store which piece each is using. This information will be what ultimately tells PlayGame which symbol to inject into the HTML and ary. 
     2c. - STRETCH GOAL - Create a computer AI to play against that makes random moves. 
         2ca. Take the available spaces in the ary, then randomly choose one to play.
-
-
-
-
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+const reset = document.getElementById('resetBtn');
+const start = document.getElementById('start');
+
 const Gameboard = (() => {
+    let x = 0;
+    
     //Array to hold game moves
         let ary = ['','','','','','','','',''];
     
     const cells = document.querySelectorAll('.cellP')
-
-    const resetBtn = document.getElementById('resetBtn');
 
     const logAry = document.getElementById('log');
 
@@ -23,24 +26,17 @@ const Gameboard = (() => {
         return;
     }
 
-
-    const resetGame = () => {
-        resetBtn.addEventListener('click', () => {
-            startGame();
-            })
-    };
-
     const log = () => {
         logAry.addEventListener('click', () => {
             console.log(ary)
         })
     };
 
-    return { checkForWinner, ary, resetGame, log };
+    return { checkForWinner, ary, log, startGame, x };
 })();
 
 const PlayGame = (() => {
-    let x;
+    let x = 0;
 
     //Query selecter to listen to all game cells.
     const cells = document.querySelectorAll('.cell')
@@ -70,78 +66,154 @@ const PlayGame = (() => {
     const click = () => {
         cells.forEach(cell => {
             cell.addEventListener('click', () => {
-                if(cell.id === cellOne.id) {
-                    if (Gameboard.ary[0] == '') {
-                    Gameboard.ary.splice(0, 1, 'X');
-                    cellP1.innerHTML = `${Gameboard.ary[0].toString()}`;
-                    } else {
-                        return;
+                if (Gameboard.x == 0) {
+                    if(cell.id === cellOne.id) {
+                        if (Gameboard.ary[0] == '') {
+                        Gameboard.ary.splice(0, 1, 'X');
+                        cellP1.innerHTML = `${Gameboard.ary[0].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 1;
+                    } else if (cell.id === cellTwo.id) {
+                        if (Gameboard.ary[1] == '') {
+                        Gameboard.ary.splice(1, 1, 'X');
+                        cellP2.innerHTML = `${Gameboard.ary[1].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 1;
+                    } else if (cell.id === cellThree.id) {
+                        if (Gameboard.ary[2] == '') {
+                        Gameboard.ary.splice(2, 1, 'X');
+                        cellP3.innerHTML = `${Gameboard.ary[2].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 1;
+                    } else if (cell.id === cellFour.id) {
+                        if (Gameboard.ary[3] == '') {
+                        Gameboard.ary.splice(3, 1, 'X');
+                        cellP4.innerHTML = `${Gameboard.ary[3].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 1;
+                    } else if (cell.id === cellFive.id) {
+                        if (Gameboard.ary[4] == '') {
+                        Gameboard.ary.splice(4, 1, 'X');
+                        cellP5.innerHTML = `${Gameboard.ary[4].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 1;
+                    } else if (cell.id === cellSix.id) {
+                        if (Gameboard.ary[5] == '') {
+                        Gameboard.ary.splice(5, 1, 'X');
+                        cellP6.innerHTML = `${Gameboard.ary[5].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 1;
+                    } else if (cell.id === cellSeven.id) {
+                        if (Gameboard.ary[6] == '') {
+                        Gameboard.ary.splice(6, 1, 'X');
+                        cellP7.innerHTML = `${Gameboard.ary[6].toString()}`; 
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 1;
+                    } else if (cell.id === cellEight.id) {
+                        if (Gameboard.ary[7] == '') {
+                        Gameboard.ary.splice(7, 1, 'X');
+                        cellP8.innerHTML = `${Gameboard.ary[7].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 1;
+                    } else if (cell.id === cellNine.id) {
+                        if (Gameboard.ary[8] == '') {
+                        Gameboard.ary.splice(8, 1, 'X');
+                        cellP9.innerHTML = `${Gameboard.ary[8].toString()}`; 
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 1;
                     }
-                    x = 0;
-                } else if (cell.id === cellTwo.id) {
-                    if (Gameboard.ary[1] == '') {
-                    Gameboard.ary.splice(1, 1, 'X');
-                    cellP2.innerHTML = `${Gameboard.ary[1].toString()}`;
-                    } else {
-                        return;
+                } else if (Gameboard.x == 1) {
+                    if(cell.id === cellOne.id) {
+                        if (Gameboard.ary[0] == '') {
+                        Gameboard.ary.splice(0, 1, 'O');
+                        cellP1.innerHTML = `${Gameboard.ary[0].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 0;
+                    } else if (cell.id === cellTwo.id) {
+                        if (Gameboard.ary[1] == '') {
+                        Gameboard.ary.splice(1, 1, 'O');
+                        cellP2.innerHTML = `${Gameboard.ary[1].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 0;
+                    } else if (cell.id === cellThree.id) {
+                        if (Gameboard.ary[2] == '') {
+                        Gameboard.ary.splice(2, 1, 'O');
+                        cellP3.innerHTML = `${Gameboard.ary[2].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 0;
+                    } else if (cell.id === cellFour.id) {
+                        if (Gameboard.ary[3] == '') {
+                        Gameboard.ary.splice(3, 1, 'O');
+                        cellP4.innerHTML = `${Gameboard.ary[3].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 0;
+                    } else if (cell.id === cellFive.id) {
+                        if (Gameboard.ary[4] == '') {
+                        Gameboard.ary.splice(4, 1, 'O');
+                        cellP5.innerHTML = `${Gameboard.ary[4].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 0;
+                    } else if (cell.id === cellSix.id) {
+                        if (Gameboard.ary[5] == '') {
+                        Gameboard.ary.splice(5, 1, 'O');
+                        cellP6.innerHTML = `${Gameboard.ary[5].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 0;
+                    } else if (cell.id === cellSeven.id) {
+                        if (Gameboard.ary[6] == '') {
+                        Gameboard.ary.splice(6, 1, 'O');
+                        cellP7.innerHTML = `${Gameboard.ary[6].toString()}`; 
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 0;
+                    } else if (cell.id === cellEight.id) {
+                        if (Gameboard.ary[7] == '') {
+                        Gameboard.ary.splice(7, 1, 'O');
+                        cellP8.innerHTML = `${Gameboard.ary[7].toString()}`;
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 0;
+                    } else if (cell.id === cellNine.id) {
+                        if (Gameboard.ary[8] == '') {
+                        Gameboard.ary.splice(8, 1, 'O');
+                        cellP9.innerHTML = `${Gameboard.ary[8].toString()}`; 
+                        } else {
+                            return;
+                        }
+                        Gameboard.x = 0;
                     }
-                    x = 1;
-                } else if (cell.id === cellThree.id) {
-                    if (Gameboard.ary[2] == '') {
-                    Gameboard.ary.splice(2, 1, 'X');
-                    cellP3.innerHTML = `${Gameboard.ary[2].toString()}`;
-                    } else {
-                        return;
-                    }
-                    x = 2;
-                } else if (cell.id === cellFour.id) {
-                    if (Gameboard.ary[3] == '') {
-                    Gameboard.ary.splice(3, 1, 'X');
-                    cellP4.innerHTML = `${Gameboard.ary[3].toString()}`;
-                    } else {
-                        return;
-                    }
-                    x = 3;
-                } else if (cell.id === cellFive.id) {
-                    if (Gameboard.ary[4] == '') {
-                    Gameboard.ary.splice(4, 1, 'X');
-                    cellP5.innerHTML = `${Gameboard.ary[4].toString()}`;
-                    } else {
-                        return;
-                    }
-                    x = 4;
-                } else if (cell.id === cellSix.id) {
-                    if (Gameboard.ary[5] == '') {
-                    Gameboard.ary.splice(5, 1, 'X');
-                    cellP6.innerHTML = `${Gameboard.ary[5].toString()}`;
-                    } else {
-                        return;
-                    }
-                    x = 5;
-                } else if (cell.id === cellSeven.id) {
-                    if (Gameboard.ary[6] == '') {
-                    Gameboard.ary.splice(6, 1, 'X');
-                    cellP7.innerHTML = `${Gameboard.ary[6].toString()}`; 
-                    } else {
-                        return;
-                    }
-                    x = 6;
-                } else if (cell.id === cellEight.id) {
-                    if (Gameboard.ary[7] == '') {
-                    Gameboard.ary.splice(7, 1, 'X');
-                    cellP8.innerHTML = `${Gameboard.ary[7].toString()}`;
-                    } else {
-                        return;
-                    }
-                    x = 7;
-                } else if (cell.id === cellNine.id) {
-                    if (Gameboard.ary[8] == '') {
-                    Gameboard.ary.splice(8, 1, 'X');
-                    cellP9.innerHTML = `${Gameboard.ary[8].toString()}`; 
-                    } else {
-                        return;
-                    }
-                    x = 8;
                 }
             })
         })
@@ -149,11 +221,55 @@ const PlayGame = (() => {
     return { click, x };
 })();
 
-const Players = (name, piece) => {
+const Player = (name, piece) => {
+    const p1Btn = document.getElementById('p1Name');
+    const p2Btn = document.getElementById('p2Name');
 
+    const getName = () => {
+        name1 = document.getElementById('name1').value;
+        name2 = document.getElementById('name2').value;
+        return name1, name2;
+    };
+
+    const p1p = () => {
+        if (document.getElementById('x').checked) {
+            piece = 'x'
+        } else if (document.getElementById('o').checked) {
+            piece = 'o'
+        }
+        return piece;
+    }
+
+    const p2p = () => {
+        if (p1Piece == 'x') {
+            piece = 'o';
+        } else {
+            piece = 'x';
+        }
+        return piece;
+    }
+
+    const p1Piece = p1p();
+
+    const p2Piece = p2p();
+    
+    return { name, p1Piece, p2Piece }
+
+    
 }
-
-
-Gameboard.resetGame();
 Gameboard.log();
 PlayGame.click();
+
+reset.addEventListener('click', () => {
+    Gameboard.startGame();
+    Gameboard.x = 0;
+    p1 = Player(document.getElementById('name1').value);
+    p2 = Player(document.getElementById('name2').value);
+    return p1, p2;
+})
+
+start.addEventListener('click', () => {
+    p1 = Player(document.getElementById('name1').value);
+    p2 = Player(document.getElementById('name2').value);
+    Gameboard.startGame();
+})
